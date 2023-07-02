f2d120e8c3b890e0cad7fab04bcb424150d6c6d0
Пипселфмейд <sergo_vzp@mail.ru>
2023-07-02 15:05:34 +0300

    ТЗ-13 исправления ревьюера

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -2,9 +2,11 @@ package ru.practicum.shareit.item;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.ItemDto;
 
 import javax.validation.Valid;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/items")
@@ -13,37 +15,37 @@ public class ItemController {
     private final ItemService service;
 
     @PostMapping
-    public ru.practicum.shareit.item.ItemDto createItem(@RequestBody @Valid ru.practicum.shareit.item.ItemDto itemDto,
+    public ItemDto createItem(@RequestBody @Valid ItemDto itemDto,
                                                         @RequestHeader("X-Sharer-User-Id") Long userId) {
         return service.createItem(itemDto, userId);
     }
 
     @GetMapping("/{itemId}")
-    public ru.practicum.shareit.item.ItemDto getItem(@PathVariable @Valid Long itemId,
+    public ItemDto getItem(@PathVariable @Valid Long itemId,
                                                      @RequestHeader("X-Sharer-User-Id") Long userId) {
         return service.getItem(itemId, userId);
     }
 
     @GetMapping("/search")
-    public List<ru.practicum.shareit.item.ItemDto> searchItem(@RequestParam String text,
+    public List<ItemDto> searchItem(@RequestParam String text,
                                                               @RequestHeader("X-Sharer-User-Id") Long userId) {
         return service.searchItem(text, userId);
     }
 
     @GetMapping
-    public List<ru.practicum.shareit.item.ItemDto> getAllItems(@RequestHeader("X-Sharer-User-Id") Long userId) {
+    public List<ItemDto> getAllItems(@RequestHeader("X-Sharer-User-Id") Long userId) {
         return service.getAllItems(userId);
     }
 
     @PatchMapping("/{itemId}")
-    public ru.practicum.shareit.item.ItemDto updateItem(@RequestBody ru.practicum.shareit.item.ItemDto itemDto,
+    public ItemDto updateItem(@RequestBody ItemDto itemDto,
                                                         @PathVariable Long itemId,
                                                         @RequestHeader("X-Sharer-User-Id") Long userId) {
         return service.updateItem(itemDto, itemId, userId);
     }
 
     @DeleteMapping("/{itemId}")
-    public ru.practicum.shareit.item.ItemDto deleteItem(@PathVariable Long itemId,
+    public ItemDto deleteItem(@PathVariable Long itemId,
                                                         @RequestHeader("X-Sharer-User-Id") Long userId) {
         return service.deleteItem(itemId, userId);
     }
