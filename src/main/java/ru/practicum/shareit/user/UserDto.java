aa8999cd6bc20db69616e9777226a281414dee33
Пипселфмейд <sergo_vzp@mail.ru>
2023-07-14 22:09:24 +0300

    ТЗ-14 Изменение dto

--- src/main/java/ru/practicum/shareit/user/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/UserDto.java
@@ -1,17 +1,19 @@
 package ru.practicum.shareit.user;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 
 @Data
-@AllArgsConstructor
+@Builder
 public class UserDto {
     private Long id;
+
     @NotNull
     private String name;
+
     @NotNull
     @Email
     private String email;
