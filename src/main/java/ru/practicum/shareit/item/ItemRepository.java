494ede2349176bb0f1ac37b39d281a0cade2fb6f
Пипселфмейд <sergo_vzp@mail.ru>
2023-07-24 22:51:50 +0300

    ТЗ-15 Исправление замечаний ревьюера

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     Collection<Item> findAllByOwnerId(Long userId);
 
+    @Query("select i from Item i where (lower(i.name) like lower(concat('%', ?1, '%')) " +
+            "or lower(i.description) like lower(concat('%', ?1, '%'))) and i.available=true")
     Page<Item> findByNameOrDescriptionAvailable(String text, Pageable pageable);
 
     List<Item> findAllByRequestId(Long requestId);
